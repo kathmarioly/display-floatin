f5602f9280f724f52694d3377732455672d1108f
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-19 16:56:29 -0400

    Uploading files

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function displayNone(){
+	/*GetElementsById me retrona una colecoón de HTML elementos
+	en esta colecciónt
+	var cajas = document.getElementsByClassName("caja");
+	cajas.forEach(function(e){
+		e.addEventListener("click", function(){
+			this.classList.toggle("no");
+		})
+	})
+
+
+})()
